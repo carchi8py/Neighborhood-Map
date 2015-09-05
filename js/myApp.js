38a1f4f7eebd4eb5c0d40f60cc88423311655a02
Chris Archibald <carchi8py@gmail.com>
2015-09-04 18:59:15 -0700

    Added a single point to the map, List is still broken

---
+++ js/myApp.js
@@ -0,0 +1,48 @@
+console.log('hii');
+var Location = function(title, latitude, longitude) {
+	//To remeber the parent context
+	var self = this;
+
+	self.title = ko.observable(title);
+	self.latitude = ko.observable(latitude);
+	self.longitude = ko.observable(longitude);
+
+	self.marker = new google.maps.Marker({
+		position: new google.maps.LatLng(self.latitude(), self.longitude()),
+		map: map
+	});
+
+	self.infoWindow = function() {
+		infowindow.setContent(self.content);
+		infowindow.open(map, self.marker);
+		$('#placeslist-switcher').attr('checked', false);
+	}
+
+	google.maps.event.addListener(self.marker, 'click', self.infoWindow);
+
+};
+
+var ViewModel = function() {
+	//To remeber the parent context
+	var self = this;
+
+	self.locations = ko.observableArray(
+		[
+			new Location('Gordon Ramsay Burgr', 36.1108308, -115.1722186)
+		]
+	);
+
+	self.openInfoWindow = function(obj) {
+		obj.infoWindow();
+	};
+};
+
+var map, infowindow;
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 36.115, lng: -115.172},
+		zoom: 16
+	});
+	infowindow = new google.maps.InfoWindow();
+	ko.applyBindings(new ViewModel());
+}
