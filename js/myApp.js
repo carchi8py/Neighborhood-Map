e9f1156f8b55b2a87d80f80a3a1e26156d8057a8
Chris Archibald <carchi8py@gmail.com>
2015-09-05 18:40:56 -0700

    Added special marker icon for the map

--- js/myApp.js
+++ js/myApp.js
@@ -1,5 +1,5 @@
 console.log('hii');
-var Location = function(title, latitude, longitude) {
+var Location = function(title, latitude, longitude, icon) {
 	//To remeber the parent context
 	var self = this;
 
@@ -7,10 +7,12 @@ var Location = function(title, latitude, longitude) {
 	self.content = '<h2 class="info-title">' + title + '</h2>'
 	self.latitude = ko.observable(latitude);
 	self.longitude = ko.observable(longitude);
+	self.icon = ko.observable(icon)
 
 	self.marker = new google.maps.Marker({
 		position: new google.maps.LatLng(self.latitude(), self.longitude()),
-		map: map
+		map: map,
+		icon: self.icon()
 	});
 
 	self.infoWindow = function() {
@@ -28,11 +30,19 @@ var ViewModel = function() {
 
 	self.locations = ko.observableArray(
 		[
-			new Location('Gordon Ramsay Burgr', 36.1108308, -115.1722186),
-			new Location('Gordon Ramsay Pub and Grill', 36.1174613, -115.175927),
-			new Location('Picasso', 36.1133574, -115.1750467),
-			new Location('Holsteins', 36.1098873, -115.1745552),
-			new Location('Nine Fine Irishmen', 36.1021012,-115.1737873)
+			new Location('Gordon Ramsay Burgr', 36.1108308, -115.1722186, 'img/lib/restaurant.png'),
+			new Location('Gordon Ramsay Pub and Grill', 36.1174613, -115.175927, 'img/lib/restaurant.png'),
+			new Location('Picasso', 36.1133574, -115.1750467, 'img/lib/restaurant.png'),
+			new Location('Holsteins', 36.1098873, -115.1745552, 'img/lib/restaurant.png'),
+			new Location('Nine Fine Irishmen', 36.1021012, -115.1737873, 'img/lib/bar.png'),
+			//Shows
+			new Location('Cirque du Soleil: Zarkana', 36.1060481, -115.1773626, 'img/lib/theater.png'),
+			new Location('Cirque du Soleil: Zumanity', 36.102895, -115.1748606, 'img/lib/stripclub2.png'),
+			new Location('Cirque du Soleil: O', 36.113911, -115.1773742, 'img/lib/theater.png'),
+			new Location('Cirque du Soleil: Beatles Love', 36.1202598, -115.1748707, 'img/lib/music_live.png'),
+			new Location('Cirque du Soleil: Mystere', 36.1244956, -115.1725308, 'img/lib/theater.png'),
+			new Location('Cirque du Soleil: Ka', 36.10324, -115.1702839, 'img/lib/theater.png'),
+			new Location('Cirque du Soleil: Criss Angel Believe', 36.0946657, -115.1774926, 'img/lib/music_live.png')
 		]
 	);
 
